47d6ccb94724a052583bede789ac864119dfb118
Tobias <tobias.lehman@elev.ga.ntig.se>
2020-09-30 10:29:45 +0200

    Behöver hjälp förstår ingenting

--- consolesweeper/Program.cs
+++ consolesweeper/Program.cs
@@ -6,7 +6,33 @@ namespace consolesweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("This is minesweeper, you will see a board with many different positions. In 10 of them there are mines that you must find.");
+
+            SweeperGrid grid1 = new SweeperGrid();
+
+            grid1.Draw();
+            while(grid1.board== 10)
+            System.Console.WriteLine("Now try to guess where the mines are, you do this by first writing an x value.");
+
+            string x = Console.ReadLine();
+
+            int x2 = int.Parse(x);
+
+            System.Console.WriteLine("Good, now write a y value.");
+
+            string y = Console.ReadLine();
+
+            int y2 = int.Parse(y);
+
+            grid1.CheckPosition(x2, y2);
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
--- consolesweeper/SweeperGrid.cs
+++ consolesweeper/SweeperGrid.cs
@@ -4,10 +4,9 @@ namespace consolesweeper
 {
     public class SweeperGrid
     {
-        public int[,] board = new int[5, 5];
+        public int[,] board = new int[10, 10];
 
         Random generator = new Random();
-
 
 
         public SweeperGrid()
@@ -17,25 +16,25 @@ namespace consolesweeper
                 for (int x = 0; x < board.GetLength(1); x++)
                 {
                     board[x, y] = 9;
+
+                    int x1 = generator.Next(1,11);
+                    int y1 = generator.Next(1,11);
+
+                    if (board[x, y] == board[x1, y1]){
+                        board[x1 ,y1] = 10;
+                    }
                 }
-            }
-            for (int n = 0; n < 10; n++)
-            {
-                int ypos = genrator.Next(1, 11);
 
-                int xpos = generator.Next(1, 11);
-
-                board[xpos, ypos] = 10;
             }
         }
 
-        public void CheckPosition(int x1, int x2)
+        public void CheckPosition(int x2, int y2)
         {
-            if (board[x1, x2] == 9)
+            if (board[x2, y2] == 9)
             {
                 System.Console.WriteLine("false");
             }
-            else if (board[x1, x2] == 10)
+            else if (board[x2, y2] == 10)
             {
                 System.Console.WriteLine("true");
             }
@@ -45,10 +44,11 @@ namespace consolesweeper
         {
             for (int y = 0; y < board.GetLength(1); y++)
             {
-                for (int x = 0; x < board.GetLength; x++)
+                for (int x = 0; x < board.GetLength(1); x++)
                 {
-                    
+                    Console.Write(board[x,y]);
                 }
+                System.Console.WriteLine();
             }
         }
     }
